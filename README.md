263c2aebdec60e64b2a3d1403256ac107d658654
GreuceanuV <imystery3d@gmail.com>
2026-06-12 20:12:10 +0300

    Deploy iPhone alarm clock app (batch 3)

---
+++ README.md
@@ -0,0 +1,9 @@
+# YouTube Alarm Clock (iPhone)
+
+## Install on iPhone
+1. Open the GitHub Pages link in **Safari**
+2. Tap **Share**
+3. Tap **Add to Home Screen**
+4. Allow **Notifications**
+
+Works like an app from your home screen.
---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "yt-alarm-v1";
+const ASSETS = ["./", "./index.html", "./css/style.css", "./js/app.js", "./js/alarms.js", "./js/storage.js", "./js/youtube.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
